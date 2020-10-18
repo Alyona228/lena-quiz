d4812b5d844a5cede87a64d43ebd87f96fada9e8
Name <56693963+Alyona228@users.noreply.github.com>
2020-10-18 21:16:23 +0300

    Github pages

---
+++ build/precache-manifest.5fd58f181bc4871590e3e28a3ce21e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a5de1dd93e6376c6a2c95d0f2bfff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002c5e8d99a36d414f10",
+    "url": "/static/css/main.57afd900.chunk.css"
+  },
+  {
+    "revision": "4d9dac58e3fa69e69274",
+    "url": "/static/js/2.fc14b81f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc14b81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002c5e8d99a36d414f10",
+    "url": "/static/js/main.c7be0227.chunk.js"
+  },
+  {
+    "revision": "8d1b1953c643d2193948",
+    "url": "/static/js/runtime-main.c1dc5470.js"
+  }
+]);
